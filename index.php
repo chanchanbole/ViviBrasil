39340aae32af98959ef2133b2fc48d49af9ea929
Chanchanbole <maxiromero75@hotmail.com>
2020-09-09 17:35:00 -0300

    Init ViviBrasil en Git

---
+++ index.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/hover.css">
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display|Montserrat:400" rel="stylesheet">
+
+    <title>ViviBrasil</title>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131436708-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-131436708-1');
+    </script>
+    
+  </head>
+  <body>
+    <section class="container">
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-center">
+        <a class="navbar-brand" href="http://buenosaires.itamaraty.gov.br/es-es/comite_visite_brasil.xml" target="_blank">
+        <h5>Comité Visite Brasil</h5>
+        </a>
+        <!--<img src="img/embajada.png" width="200" height="auto" alt="embajada">-->
+        </nav>
+        
+        <header>
+            <div class="header row d-flex flex-row justify-content-start align-items-center">
+                    
+                <img src="img/veni-a-brasil.png" alt="venabrasil" class="img-01 col-xl-6">
+                <div class="copy text-center col-xl-12 text-white"><h3>¡Aprovechá ofertas y beneficios exclusivos a Brasil!</h3></div>
+                
+            </div>
+            
+
+        </header>
+
+        <section class="card-columns my-3" id="galeria">
+
+            <div class="card">
+                <a href="http://www.halifaxviajes.com/promos_view.php?wVarID=204" target="_blank" class="hvr-bob">
+                <img src="img/1.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="http://www.ilnet.com.ar/net/emisivo/abrasil/1057_BRASILENPESOS/1057_BRASILENPESOS.html" target="_blank" class="hvr-bob">
+                <img src="img/2.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="http://www.mediterraneaturismo.tur.ar" target="_blank" class="hvr-bob">
+                <img src="img/3.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="http://www.bonbini.tur.ar/" target="_blank" class="hvr-bob">
+                <img src="img/4.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="https://www.shoppingviajes.com/espanol/tematica/429/Gran-Barata-de-Paquetes/" target="_blank" class="hvr-bob">
+                <img src="img/5.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="https://www.latam.com/es_ar/latam-travel/paquetes-turisticos/brasil/" target="_blank" class="hvr-bob">
+                <img src="img/6.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="https://juantoselli.com/salidas/quotas/?query=b%C3%BAzios" target="_blank" class="hvr-bob">
+                <img src="img/7.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="http://claseturista.tur.ar/contacto" target="_blank" class="hvr-bob">
+                <img src="img/8.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="http://www.cht.com.ar/paquetes/NATAL/NATAL-SALIDA-19-DE-ENERO/destino-72-5699.html" target="_blank" class="hvr-bob">
+                <img src="img/9.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="https://www.eurovips.com/" target="_blank" class="hvr-bob">
+                <img src="img/10.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="http://www.freeway.com.ar " target="_blank" class="hvr-bob">
+                <img src="img/11.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="https://www.universal-assistance.com/ar-es/home.html" target="_blank" class="hvr-bob">
+                <img src="img/12.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+             
+        <!--<script>
+        
+         var imagenes = [1,2,3,4,5,6,7,8,9,10,11];
+         var galeria = document.getElementById('galeria');
+
+         for(imagen of imagenes){
+             galeria.innerHTML += `
+             <div class="card">
+                <a href="#" data-toggle="modal" data-target="#img_galery${imagen}">
+                <img src="img/${imagen}.png" alt="" class="card-img-top">
+                </a>
+            </div>
+
+                `
+         }
+
+        </script>
+        -->
+        
+        </section>
+
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
